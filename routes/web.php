e7b6eaf381bb6dc4ed0824a651e37211947859e5
Richard Reinhart <richard.reinhart@enseval.com>
2024-09-30 11:20:06 +0700

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
+Route::get('/items/store', [ItemController::class, 'store'])->name('items.store');
